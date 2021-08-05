63b4d7c3e7498da43344137f76e92fc2c0cf9965
Mudassir <mudassiraliansari@gmail.com>
2021-08-05 17:50:39 +0500

    added Mine in code

--- README.md
+++ README.md
@@ -66,3 +66,15 @@ REPLACE
 Magento\Framework\App\View\Asset\MaterializationStrategy\Symlink
 WITH
 Magento\Framework\App\View\Asset\MaterializationStrategy\Copy
+
+
+After override:
+php -d memory_limit=-1 bin/magento setup:upgrade
+
+php -d memory_limit=-1 bin/magento setup:di:compile
+
+php -d memory_limit=-1 bin/magento setup:static-content:deploy
+
+php -d memory_limit=-1 bin/magento cache:clean
+
+php -d memory_limit=-1 bin/magento cache:flush
--- generated/code/Magento/Catalog/Model/ProductRepository/Interceptor.php
+++ generated/code/Magento/Catalog/Model/ProductRepository/Interceptor.php
@@ -17,9 +17,63 @@ class Interceptor extends \Magento\Catalog\Model\ProductRepository implements \M
     /**
      * {@inheritdoc}
      */
+    public function get($sku, $editMode = false, $storeId = null, $forceReload = false)
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'get');
+        return $pluginInfo ? $this->___callPlugins('get', func_get_args(), $pluginInfo) : parent::get($sku, $editMode, $storeId, $forceReload);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getById($productId, $editMode = false, $storeId = null, $forceReload = false)
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getById');
+        return $pluginInfo ? $this->___callPlugins('getById', func_get_args(), $pluginInfo) : parent::getById($productId, $editMode, $storeId, $forceReload);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function save(\Magento\Catalog\Api\Data\ProductInterface $product, $saveOptions = false)
     {
         $pluginInfo = $this->pluginList->getNext($this->subjectType, 'save');
         return $pluginInfo ? $this->___callPlugins('save', func_get_args(), $pluginInfo) : parent::save($product, $saveOptions);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function delete(\Magento\Catalog\Api\Data\ProductInterface $product)
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'delete');
+        return $pluginInfo ? $this->___callPlugins('delete', func_get_args(), $pluginInfo) : parent::delete($product);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteById($sku)
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'deleteById');
+        return $pluginInfo ? $this->___callPlugins('deleteById', func_get_args(), $pluginInfo) : parent::deleteById($sku);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getList(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria)
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'getList');
+        return $pluginInfo ? $this->___callPlugins('getList', func_get_args(), $pluginInfo) : parent::getList($searchCriteria);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function cleanCache()
+    {
+        $pluginInfo = $this->pluginList->getNext($this->subjectType, 'cleanCache');
+        return $pluginInfo ? $this->___callPlugins('cleanCache', func_get_args(), $pluginInfo) : parent::cleanCache();
     }
 }
